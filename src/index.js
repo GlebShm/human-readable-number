13f55209ab7ebc01d022231822e5b9a92a53990a
Gleb <glebshm@rambler.ru>
2022-07-14 22:11:00 +0300

    feat: complete human readable number algoritm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  let numbers = [' ', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+    'seventeen', 'eighteen', 'nineteen'];
+  let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let numStr = number.toString()
+  if (numStr == '0') return 'zero'
+  if (number < 20) {
+    return numbers[number]
+  } else if (numStr.length == 2) {
+    if (numStr[1] == 0) {
+      return tens[numStr[0]]
+    } else {
+      return tens[numStr[0]] + ' ' + numbers[numStr[1]]
+    }
+  } else if (numStr.length == 3) {
+    if (numStr[1] == '0' && numStr[2] == '0') {
+      return numbers[numStr[0]] + ' hundred'
+    } else {
+      return numbers[numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]))
+    }
+  }
 }
