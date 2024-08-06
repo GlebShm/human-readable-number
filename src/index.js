9bb8988b957109229fe7335fa9464993d943e3f8
Gleb <glebshm@rambler.ru>
2024-08-06 21:11:23 +0300

    complete hrn algoritm 24

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 module.exports = function toReadable(number) {
-  let numbers = [' ', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-    'seventeen', 'eighteen', 'nineteen'];
-  let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  let numStr = number.toString()
-  if (numStr == '0') return 'zero'
-  if (number < 20) {
-    return numbers[number]
-  } else if (numStr.length == 2) {
-    if (numStr[1] == 0) {
-      return tens[numStr[0]]
-    } else {
-      return tens[numStr[0]] + ' ' + numbers[numStr[1]]
-    }
-  } else if (numStr.length == 3) {
-    if (numStr[1] == '0' && numStr[2] == '0') {
-      return numbers[numStr[0]] + ' hundred'
-    } else {
-      return numbers[numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]))
-    }
+  if (number==0) return 'zero'
+  let numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 
+      'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+    let hundreds=Math.floor(number/100)
+    let dozens=Math.floor(number%100/10)
+    let units=number%10
+
+    if (number<20) {
+        return numbers[number]
+      } else if (number<100) {
+          return tens[dozens-2] + (units? ` ${numbers[units]}`: '')
+        } else if(number < 1000) {
+            let rest=` ${toReadable(number%100)}`
+            return numbers[hundreds]+' hundred'+(rest==' zero' ? '': rest)
+          }
   }
-}
